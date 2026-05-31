3a39d36b735831dd1d932d40b37e243c2d24c3ca
EmC2029 <satosugu13324@gmail.com>
2026-05-31 18:27:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("Hello from Funticing!");
-let businessName = "Functicing";
+let businessName = "Functicing"; console.log (businessName); 
+let greeting = document.getElementById("welcomeMsg");
+
+console.log(greeting);
+function showGreeting() {
+
+greeting.textContent = "Welcome to " + businessName + "!";
+
+}
+
+document.getElementById("myButton").addEventListener("click",
+showGreeting);
